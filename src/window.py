91fe312705abd4e5724ce94c344bd152cbf5b089
V-Official250 <85917376+V-Official250@users.noreply.github.com>
2021-07-20 22:33:02 +0530

    Update window.py

--- src/window.py
+++ src/window.py
@@ -1,1 +1,29 @@
+import pygame,sys
 
+class Window:
+    def __init__(self, width:int, height:int, title:str):
+        """
+        it creates a window for you application
+        """
+
+        #window's values
+        self.width:int = width
+        self.height:int = height
+        self.title:str = title
+
+        #setting up window and keys
+        self.__window:pygame.display = pygame.display.set_mode((width,height))
+        self.isOpen:bool = True
+
+        pygame.display.set_caption(title)
+
+    def clear(self,red:int,green:int,blue:int):
+        self.__window.fill((red,green,blue))
+
+    def pollevent(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.isOpen = False
+
+    def update(self):
+        pygame.display.update()
