40f58d0085ce0fe18f3655cdeed168e99e121d1b
V-Official250 <85917376+V-Official250@users.noreply.github.com>
2021-07-21 11:39:04 +0530

    Update font.py

--- src/font.py
+++ src/font.py
@@ -1,1 +1,9 @@
+import pygame
+pygame.init()
 
+class Font:
+    def __init__(self,path:str,size:int=10) -> None:
+        self.font = pygame.font.Font(path,size)
+
+    def getText(self,text:str="Text",color:tuple=(0,0,0)):
+        return self.font.render(text,True,color)
